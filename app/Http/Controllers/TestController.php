e73cb6fd651c105c514b4c6931bbd45ea666b0fc
44x1carbon <b4079@oic.jp>
2016-07-07 15:24:32 +0900

    [mod] controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -50,7 +50,7 @@ class TestController extends Controller
       {
           return response()->json('Bad Request',400);
       }
-      $value = DB::table('ToDo')->insert([
+      DB::table('ToDo')->insert([
         'todo' => Input::get('todo'),
         'flug' => false,
       ]);
